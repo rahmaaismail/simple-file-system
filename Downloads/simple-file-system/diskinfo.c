b29a6dd22773da2d1994ae8c73d71fbcab864273
Rahma Ismail <rahmaismail@uvic.ca>
2026-03-24 00:24:49 -0700

    Updated skeleton

--- Downloads/simple-file-system/diskinfo.c
+++ Downloads/simple-file-system/diskinfo.c
@@ -1,40 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "fs_utils.h"
+/* diskinfo.c
+ * Part I: Simple File System - Disk Info
+ * Lists root directory entries with creation date/time
+ */
 
-int main(int argc, char *argv[]){
-    if(argc != 2){
-        printf("Usage: %s <disk image>\n", argv[0]);
-        return 1;
-    }
-
-    FILE *fp = fopen(argv[1], "rb");
-    if(!fp){
-        perror("Failed to open disk image");
-        return 1;
-    }
-
-    char boot[512];
-    fread(boot, 1, 512, fp);
-
-    char os_name[9];
-    memcpy(os_name, boot+3, 8);
-    os_name[8] = '\0';
-
-    unsigned short bytes_per_sector = get_le16(boot+11);
-    unsigned char sectors_per_cluster = boot[13];
-    unsigned short reserved_sectors = get_le16(boot+14);
-    unsigned char num_fats = boot[16];
-    unsigned short max_root_entries = get_le16(boot+17);
-    unsigned short total_sectors = get_le16(boot+19);
-    unsigned short sectors_per_fat = get_le16(boot+22);
-
-    printf("OS Name: %s\n", os_name);
-    printf("Number of FAT copies: %d\n", num_fats);
-    printf("Sectors per FAT: %d\n", sectors_per_fat);
-    printf("Total size of disk: %d bytes\n", total_sectors * bytes_per_sector);
-
-    fclose(fp);
-    return 0;
-}
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <fcntl.h>
+ #include <unistd.h>
+ #include <sys/mman.h>
+ #include <sys/stat.h>
+ #include "fs_utils.h"  // contains print_date_time()
+ 
+ #define DISK_PATH "mydisk"   // change to your disk file
+ #define ROOT_DIR_OFFSET 0x2600 // adjust if needed
+ #define ENTRY_SIZE 32         // standard FAT directory entry size
+ 
+ void list_root_directory(char *disk_start) {
+     char *root_dir = disk_start + ROOT_DIR_OFFSET;
+ 
+     printf("Root Directory Entries:\n");
+ 
+     for (int i = 0; i < 16; i++) { // list 16 entries as example
+         char *entry = root_dir + i * ENTRY_SIZE;
+ 
+         // check if entry is used (not empty)
+         if (entry[0] == 0x00) continue;
+ 
+         printf("Entry %d: ", i + 1);
+         // print file name (first 11 bytes)
+         for (int j = 0; j < 11; j++) {
+             putchar(entry[j]);
+         }
+         putchar(' ');
+ 
+         // print creation date/time using helper
+         print_date_time(entry);
+     }
+ }
+ 
+ int main() {
+     int fd = open(DISK_PATH, O_RDONLY);
+     if (fd < 0) {
+         perror("open");
+         return 1;
+     }
+ 
+     struct stat st;
+     if (fstat(fd, &st) < 0) {
+         perror("fstat");
+         return 1;
+     }
+ 
+     char *disk = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+     if (disk == MAP_FAILED) {
+         perror("mmap");
+         return 1;
+     }
+ 
+     list_root_directory(disk);
+ 
+     munmap(disk, st.st_size);
+     close(fd);
+ 
+     return 0;
+ }
--- Downloads/simple-file-system/fs_utils.c
+++ Downloads/simple-file-system/fs_utils.c
@@ -1,41 +1,26 @@
-#include "fs_utils.h"
-#include <ctype.h>
-#include <string.h>
+/* fs_utils.c
+ * Implementation of helper functions for Simple File System
+ * Author: Huan
+ */
 
-#define timeOffset 14
-#define dateOffset 16
-
-// Convert 2 bytes little endian to unsigned short
-unsigned short get_le16(char *ptr){
-    return (unsigned char)ptr[0] | ((unsigned char)ptr[1] << 8);
-}
-
-// Convert 4 bytes little endian to unsigned int
-unsigned int get_le32(char *ptr){
-    return (unsigned char)ptr[0] |
-           ((unsigned char)ptr[1] << 8) |
-           ((unsigned char)ptr[2] << 16) |
-           ((unsigned char)ptr[3] << 24);
-}
-
-// Print FAT12 creation date/time
-void print_date_time(char * directory_entry_startPos){
-    unsigned short time = get_le16(directory_entry_startPos + timeOffset);
-    unsigned short date = get_le16(directory_entry_startPos + dateOffset);
-
-    int year  = ((date & 0xFE00) >> 9) + 1980;
-    int month = (date & 0x1E0) >> 5;
-    int day   = (date & 0x1F);
-
-    int hours   = (time & 0xF800) >> 11;
-    int minutes = (time & 0x7E0) >> 5;
-
-    printf("%04d-%02d-%02d %02d:%02d", year, month, day, hours, minutes);
-}
-
-// Convert string to uppercase (for FAT12 filenames)
-void strtoupper(char *str, int len){
-    for(int i=0; i<len; i++){
-        str[i] = toupper((unsigned char)str[i]);
-    }
-}
+ #include <stdio.h>
+ #include "fs_utils.h"
+ 
+ #define TIME_OFFSET 14  // offset of creation time in directory entry
+ #define DATE_OFFSET 16  // offset of creation date in directory entry
+ 
+ void print_date_time(char *directory_entry_startPos) {
+     unsigned short time = *(unsigned short *)(directory_entry_startPos + TIME_OFFSET);
+     unsigned short date = *(unsigned short *)(directory_entry_startPos + DATE_OFFSET);
+ 
+     // extract date components
+     int year = ((date & 0xFE00) >> 9) + 1980;
+     int month = (date & 0x1E0) >> 5;
+     int day = date & 0x1F;
+ 
+     // extract time components
+     int hours = (time & 0xF800) >> 11;
+     int minutes = (time & 0x7E0) >> 5;
+ 
+     printf("%04d-%02d-%02d %02d:%02d\n", year, month, day, hours, minutes);
+ }
